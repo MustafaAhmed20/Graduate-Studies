7bee7cfdca57dc3e9d5b3c4eb559270c2e0e46a8
Mustafa Ahmed <mmustafaadn@gmail.com>
2019-03-12 21:44:27 +0200

    add data base migrate to 'manage.py'

--- manage.py
+++ manage.py
@@ -2,8 +2,12 @@
 
 import os
 from app import create_app, db
+from flask_migrate import Migrate
+
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
 	app.run()
