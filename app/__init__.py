d202a3c48c065e70bf41241ad66a04d1c893ca22
Mustafa Ahmed <mmustafaadn@gmail.com>
2019-03-12 21:39:32 +0200

    add global helpful functions

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,17 @@ from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 
+from flask_login import LoginManager
+
 
 from config import config
 
 db = SQLAlchemy()
+
+# auth
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
 	app = Flask(__name__)
@@ -17,12 +24,17 @@ def create_app(config_name):
 	# bootstrap.init_app(app)
 	# mail.init_app(app)
 	# moment.init_app(app)
+	
 	db.init_app(app)
-	Session(app)
+	login_manager.init_app(app)
+	#Session(app)
 
 	# attach routes and custom error pages here
 	from .main import main as main_blueprint
+	from .auth import auth as auth_blueprint
+
 	app.register_blueprint(main_blueprint)
+	app.register_blueprint(auth_blueprint)
 	
 	return app
 
@@ -32,3 +44,74 @@ def _default(self, obj):
 
 _default.default = JSONEncoder().default
 JSONEncoder.default = _default
+
+def validation(field, Type):
+	""" vlaidation function .
+		type = username the length between(5-28)
+		type = password the length between(5-28)
+		type = phone the length between(10-14) and is digit
+		type = email takes the email shape
+		type = digit must be numbers"""
+
+	if not field:
+		return False
+
+	elif Type == "username" or 'password':
+		if not (5 <= len(field) <= 28):
+			return False
+
+	elif Type == "phone":
+		if not (10 <= len(field) <= 14) or not (field.isdigit()):
+			return False
+
+	elif Type == "digit":
+		if not field.isdigit():
+			return False
+	
+	elif Type == "email":
+		
+		import re
+		email ='info@domain.com'
+		match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+
+		if match == None:
+			return False
+	
+	else :
+		# not valid type
+		raise KeyError("not valid parameter 'Type' .")
+
+	return True
+
+# response model
+def gResponse(Type='noLink', status=True, message='', link=''): 
+	""" global response model with two types:
+		- with link
+		- no link
+		..
+		parameters :
+		. Type
+		. status
+		. message
+		. link
+		"""
+	if status not in (True, False):
+		raise KeyError("not valid parameter 'status' .")
+	if not type(message) is str:
+		raise KeyError("not valid parameter 'message' .")
+	if not type(link) is str:
+		raise KeyError("not valid parameter 'link' .")
+
+	if status:
+		status = 'success'
+	else:
+		status = 'error'
+	
+	if Type == 'noLink':
+		return {"status": status, 'message': message}
+	elif Type == 'withLink':
+		return {"status": status, 'link': link}
+	else:
+		raise KeyError("not valid parameter 'Type' .")
+
+
